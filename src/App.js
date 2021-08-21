821a720608984fe92ab5f3386406d8462af266c6
irisdew <ycleardew@gmail.com>
2021-08-21 22:10:34 +0900

    이슬:: 제안된 프로젝트 목록 상단 UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GlobalStyle from 'styles/GlobalStyle';
+import Header from 'components/Header';
 import Home from 'pages//Home';
 import Login from 'pages/Login';
 import SignUp from 'pages/SignUp';
 import EmailSignUp from 'pages/EmailSignUp';
-import Header from 'components/Header';
+import ProjectList from 'pages/ProjectList';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
         <Route path="/signup/email" component={EmailSignUp} />
+        <Route exact path="/project" component={ProjectList} />
       </Switch>
     </Router>
   );
---
+++ src/components/Search.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BsSearch } from 'react-icons/bs';
+
+const Search = () => {
+  return (
+    <Wrapper>
+      <SearchIcon size={20} />
+      <Input type="text" placeholder="Search..." />
+    </Wrapper>
+  );
+};
+
+export default Search;
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const SearchIcon = styled(BsSearch)`
+  position: absolute;
+  left: 25px;
+  top: 20px;
+`;
+
+const Input = styled.input`
+  padding: 5px 7px 5px 45px;
+  margin: 10px;
+  width: 300px;
+  height: 40px;
+  border: 1px solid #c4c4c4;
+  border-radius: 20px;
+  :focus {
+    outline: none;
+  }
+`;
