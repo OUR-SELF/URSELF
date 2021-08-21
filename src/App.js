bf8043c924091070398e8f8d5e99c5e82c3faf88
irisdew <ycleardew@gmail.com>
2021-08-22 01:03:47 +0900

    이슬:: 프로젝트 상세 페이지 UI

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from 'pages/Login';
 import SignUp from 'pages/SignUp';
 import EmailSignUp from 'pages/EmailSignUp';
 import ProjectList from 'pages/ProjectList';
+import ProjectDetail from 'pages/ProjectDetail';
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
@@ -22,6 +23,7 @@ function App() {
         <Route exact path="/signup" component={SignUp} />
         <Route path="/signup/email" component={EmailSignUp} />
         <Route exact path="/project" component={ProjectList} />
+        <Route path="/project/:id" component={ProjectDetail} />
       </Switch>
       <Footer />
     </Router>
