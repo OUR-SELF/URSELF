93e12b820f28fef7ebed529be3aab84964abd003
irisdew <ycleardew@gmail.com>
2021-08-21 19:57:25 +0900

    이슬:: 로그인 UI 추가

---
+++ src/components/SocialButton.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { FcGoogle } from 'react-icons/fc';
+
+const SocialButton = ({ type, content }) => {
+  return (
+    <>
+      <Button>
+        {type === 'google' && <FcGoogle size={18} />}
+        {type === 'kakao' && <Kakao />}
+        {type === 'naver' && <Naver />}
+
+        <Content>{content}</Content>
+      </Button>
+    </>
+  );
+};
+
+export default SocialButton;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 35px;
+  background: #ffffff;
+  border: 1px solid #c4c4c4;
+  border-radius: 10px;
+  margin: 10px 0;
+`;
+
+const Content = styled.span`
+  font-size: 12px;
+  margin-left: 15px;
+`;
+
+const Kakao = styled.div`
+  width: 18px;
+  height: 18px;
+  background-image: url('/images/kakao.png');
+  background-size: contain;
+`;
+
+const Naver = styled(Kakao)`
+  background-image: url('/images/naver.png');
+`;
