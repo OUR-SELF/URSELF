5a025c652d56c1031700c0a4a48dfff406dafe70
irisdew <ycleardew@gmail.com>
2021-08-22 21:36:19 +0900

    이슬:: 프로젝트 상세페이지 GET 연결

--- src/pages/ProjectDetail.js
+++ src/pages/ProjectDetail.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from 'styled-components/macro';
 import { FaUserCircle } from 'react-icons/fa';
 import { DiscussionEmbed } from 'disqus-react';
+import { defaultProject } from 'data';
 
-const ProjectDetail = () => {
+const ProjectDetail = ({ match }) => {
+  const { id } = match.params;
+  const [data, setData] = useState(defaultProject);
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:8000/projects/' + id)
+      .then((response) => {
+        console.log(response);
+        setData(response.data);
+      })
+      .catch((err) => console.log(err.response));
+  }, []);
   return (
     <>
       <img alt="banner" src={'/images/top-banner.jpg'} />
       <Container>
-        <Category>제안된 프로젝트 > 게임</Category>
+        <Category>제안된 프로젝트 > 카테고리 > {data.category}</Category>
         <FlexDiv>
-          <Title>언더테일(UnderTail)</Title>
+          <Title>{data.name}</Title>
           <User>
             <FaUserCircle size={20} />
             <span>김토비</span>
@@ -21,19 +34,20 @@ const ProjectDetail = () => {
           <ProjectInfo>
             <div>
               <h4>
-                마감까지 <b>24일</b> 남음
+                마감일 ~<b>{data.end_date}</b>
               </h4>
               <h4>
-                현재까지 <b>560,000원</b> 펀딩
+                현재까지 <b>{data.target_amount}원</b> 펀딩
               </h4>
               <br />
               <br />
               <br />
               <h4>
-                <span>참여자 수</span> 24명
+                <span>참여자 수</span> {data.counted_user_num}명
               </h4>
               <h4>
-                <span>희망 가격대</span>15,000원
+                <span>희망 가격대</span>
+                {data.price}원
               </h4>
             </div>
             <Button>펀딩에 참여하기</Button>
@@ -41,42 +55,15 @@ const ProjectDetail = () => {
         </FlexDiv>
         <Section>
           <SubTitle>기업에게 한 마디</SubTitle>
-          <p>
-            언더테일은 토비 폭스가 제작한 2015년 9월 15일에 출시된 인디
-            게임이다. 한국어로 '지하 이야기' 정도로 번역할 수 있겠다. 2013년부터
-            킥스타터를 통해 투자받은 금액으로 제작되었으며 릴리즈 영상 출시 이후
-            백만 장 이상이 팔리며 상업적인 성공을 거두었다. 타임지나 IGF, IGN,
-            SWSX 등의 시상식에서 상을 수상하며 평단으로부터 높은 평가를 받았다.
-            스팀과 공식 홈페이지에서 살 수 있으며, 공식 홈페이지에서 구매하면
-            스팀 키와 함께 DRM Free 버전을 받을 수 있다. 정가는
-            9.99달러(10,500원)로, 사운드트랙 포함 버전 17.99달러(19,000원)이다.
-          </p>
+          <p>{data.comment}</p>
         </Section>
         <Section>
           <SubTitle>제안의도</SubTitle>
-          <p>
-            언더테일은 토비 폭스가 제작한 2015년 9월 15일에 출시된 인디
-            게임이다. 한국어로 '지하 이야기' 정도로 번역할 수 있겠다. 2013년부터
-            킥스타터를 통해 투자받은 금액으로 제작되었으며 릴리즈 영상 출시 이후
-            백만 장 이상이 팔리며 상업적인 성공을 거두었다. 타임지나 IGF, IGN,
-            SWSX 등의 시상식에서 상을 수상하며 평단으로부터 높은 평가를 받았다.
-            스팀과 공식 홈페이지에서 살 수 있으며, 공식 홈페이지에서 구매하면
-            스팀 키와 함께 DRM Free 버전을 받을 수 있다. 정가는
-            9.99달러(10,500원)로, 사운드트랙 포함 버전 17.99달러(19,000원)이다.
-          </p>
+          <p>{data.intent}</p>
         </Section>
         <Section>
           <SubTitle>프로젝트 상세 설명</SubTitle>
-          <p>
-            언더테일은 토비 폭스가 제작한 2015년 9월 15일에 출시된 인디
-            게임이다. 한국어로 '지하 이야기' 정도로 번역할 수 있겠다. 2013년부터
-            킥스타터를 통해 투자받은 금액으로 제작되었으며 릴리즈 영상 출시 이후
-            백만 장 이상이 팔리며 상업적인 성공을 거두었다. 타임지나 IGF, IGN,
-            SWSX 등의 시상식에서 상을 수상하며 평단으로부터 높은 평가를 받았다.
-            스팀과 공식 홈페이지에서 살 수 있으며, 공식 홈페이지에서 구매하면
-            스팀 키와 함께 DRM Free 버전을 받을 수 있다. 정가는
-            9.99달러(10,500원)로, 사운드트랙 포함 버전 17.99달러(19,000원)이다.
-          </p>
+          <p>{data.details}</p>
         </Section>
         <Section>
           <SubTitle>펀딩에 참여하기</SubTitle>
